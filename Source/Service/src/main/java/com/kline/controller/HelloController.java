0fe73908d5a73733977c0f1c47ffae85d7f69081
meiguanggan <meigg@jinbenteng.com>
2018-05-03 18:32:43 +0800

    修改使用IntelliJ IDEA 的方式开发工程

---
+++ Source/Service/src/main/java/com/kline/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.kline.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public Object hello() {
+        return "Kline Hello ~~";
+    }
+}
---
+++ Source/Service/src/main/java/com/kline/pojo/JsonResult.java
@@ -0,0 +1,5 @@
+package com.kline.pojo;
+
+public class JsonResult {
+    
+}

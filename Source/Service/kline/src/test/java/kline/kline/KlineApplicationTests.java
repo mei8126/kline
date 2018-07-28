96c28e5678098224a92d4f62cc4ce808d52defcf
meiguanggan <meigg@jinbenteng.com>
2018-07-29 00:33:10 +0800

    创建新的项目工程

---
+++ Source/Service/kline/src/test/java/kline/kline/KlineApplicationTests.java
@@ -0,0 +1,16 @@
+package kline.kline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KlineApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
